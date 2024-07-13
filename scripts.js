16e18ce0de997101a0fb723081e0ace689b22025
Mohak124 <anandmohak820@gmail.com>
2024-07-13 14:17:42 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,17 @@
+function Solve(val) {
+    var v = document.getElementById('screen');
+    v.value += val;
+ }
+ function Result() {
+    var num1 = document.getElementById('screen').value;
+    var num2 = eval(num1);
+    document.getElementById('screen').value = num2;
+ }
+ function Clear() {
+    var inp = document.getElementById('screen');
+    inp.value = '';
+ }
+ function Back() {
+    var ev = document.getElementById('screen');
+    ev.value = ev.value.slice(0,-1);
+ }
